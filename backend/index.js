9eb949ea26aa0914cb45c2e1fc1f14523b591667
salman_siddeeque <salmanres@gmail.com>
2025-03-04 14:19:50 +0530

    invertis

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 let mydata = [];
 
 app.use(express.json()); //middleware
-app.use(cors());
+app.use(cors()); //middleware
 
 app.get('/', (req, res)=>{
     res.send('server');
@@ -16,8 +16,10 @@ app.post('/register', (req, res)=>{
     console.log(req.body);
     mydata.push(req.body);
     console.log('updated data is: ', mydata);
-    res.status(250).json({message: 'user registered successfully'});
+    res.status(250).json({message: 'user registered successfully', mydata});
 })
 
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+});
--- frontend/src/dashboard/RegisterPage.js
+++ frontend/src/dashboard/RegisterPage.js
@@ -5,6 +5,8 @@ function RegisterPage() {
 
     const [data, setData] = useState();
 
+    const [user, setuser] = useState([]);
+
     const getdata = (item) => {
         setData({
             ...data,
@@ -17,6 +19,7 @@ function RegisterPage() {
         const response = await axios.post('http://localhost:3500/register', data);
         console.log(response.data);
         alert(response.data.message);
+        setuser(response.data.mydata);
     }
 
     return (
@@ -30,6 +33,30 @@ function RegisterPage() {
                     <input type='password' placeholder='enter password' name='password' onInput={getdata} />
                     <button onClick={savedata}>REGISTER</button>
                 </div>
+            </div>
+            <div>
+                <table border={1} cellPadding={10} className='mytable'>
+                    <thead>
+                        <tr>
+                            <td>username</td>
+                            <td>email</td>
+                            <td>mobile</td>
+                            <td>password</td>
+                            <td>action</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {user ? user.map((item)=>(
+                            <tr>
+                                <td>{item.username}</td>
+                                <td>{item.email}</td>
+                                <td>{item.mobile}</td>
+                                <td>{item.password}</td>
+                                <td><button>edit</button><button>delete</button></td>
+                            </tr>
+                        ))  :<h1>loading...</h1>}
+                    </tbody>
+                </table>
             </div>
         </Fragment>
     )
